4d2c3f5190bdd55a2acf7efffb4b75c470d1079e
venkatarohan <venkatarohank@gmail.com>
2025-03-17 01:03:51 +0530

    updated create Expnese method

--- splitwise/src/main/java/org/splitwise/services/ExpenseService.java
+++ splitwise/src/main/java/org/splitwise/services/ExpenseService.java
@@ -1,15 +1,28 @@
 package org.splitwise.services;
 
-import org.splitwise.models.User;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import org.splitwise.models.split.ExpenseType;
 import org.splitwise.models.Expense;
 import org.splitwise.models.Split;
-
+import java.util.Map;
 
 public class ExpenseService {
-    public Expense createExpense(String title, int amount, String paidBy, String description, Split[] splits, ExpenseType expenseType, String groupId){
-        Expense expense = new Expense(title, amount, paidBy, description, splits, expenseType, groupId);
+    // SELECT * FROM expense WHERE group_id = 'group_id'
+    Map<String, List<Expense>> groupExpense;
+
+    public ExpenseService(){
+        groupExpense = new HashMap<>();
+    }
+
+    public Expense createExpense(String id,String title, int amount, String paidBy, String description, List<Split> splits, ExpenseType expenseType, String groupId){
+        Expense expense = new Expense(id, title, amount, paidBy, description, splits, expenseType, groupId);
+
+        groupExpense.putIfAbsent(groupId, new ArrayList<>());
+        groupExpense.get(groupId).add(expense);
+
         return expense;
     }
-    
+
 }
