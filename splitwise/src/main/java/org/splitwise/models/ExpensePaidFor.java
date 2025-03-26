4abf79ea8af6714d46be29e5cbbc0d59d1344e71
venkatarohan <venkatarohank@gmail.com>
2025-03-26 23:54:56 +0530

    added ExpensePaidFor model

---
+++ splitwise/src/main/java/org/splitwise/models/ExpensePaidFor.java
@@ -0,0 +1,12 @@
+package org.splitwise.models;
+
+public class ExpensePaidFor {
+    public String userId;
+    public int shares;
+
+    public ExpensePaidFor(String userId, int shares){
+        this.userId = userId;
+        this.shares = shares;
+    }
+
+}
