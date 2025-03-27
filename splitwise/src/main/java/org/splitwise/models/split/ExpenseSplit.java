7fad725960bce65c9955081d0c04cecd2e988fe6
venkatarohan <venkatarohank@gmail.com>
2025-03-27 22:43:04 +0530

    added Interface for expense validation

---
+++ splitwise/src/main/java/org/splitwise/models/split/ExpenseSplit.java
@@ -0,0 +1,14 @@
+package org.splitwise.models.split;
+
+import java.util.List;
+
+import org.splitwise.models.Split;
+
+public interface ExpenseSplit {
+    List<Split> calculateSplit();
+
+    void validate();
+}
+
+
+
