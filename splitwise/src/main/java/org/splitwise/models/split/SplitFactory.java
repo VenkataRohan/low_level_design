a0b10e62389ce1c1503eefb34f8fba9132021e91
venkatarohan <venkatarohank@gmail.com>
2025-04-01 01:08:47 +0530

    updated splitfactory return type to ExpenseSplit

--- splitwise/src/main/java/org/splitwise/models/split/SplitFactory.java
+++ splitwise/src/main/java/org/splitwise/models/split/SplitFactory.java
@@ -1,17 +1,17 @@
 package org.splitwise.models.split;
 
-import org.splitwise.models.Split;
-
+import org.splitwise.models.ExpensePaidFor;
+import java.util.List;
 
 public class SplitFactory {
-    public static Split createSplit(ExpenseType expenseType, String userId, int amount){
+    public static ExpenseSplit createSplit(ExpenseType expenseType, List<ExpensePaidFor> expensePaidFor, int amount){
         switch (expenseType) {
             case EQUAL:
-                return new EqualSplit(userId);
-            case UNEQUAL:
-                return new UnEqualSplit(userId, amount);
-            case PERCENT:
-             return new PercentSplit(userId, amount);
+                return new EqualSplit(expensePaidFor, amount);
+            // case UNEQUAL:
+            //     return new UnEqualSplit(expensePaidFor);
+            // case PERCENT:
+            //  return new PercentSplit(expensePaidFor);
             default:
                 throw new IllegalArgumentException("Invalid Expense Type");
         }
