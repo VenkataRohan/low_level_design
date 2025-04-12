468ec03c1d2ad573fbd9491f77835069830f98de
venkatarohan <venkatarohank@gmail.com>
2025-04-12 18:36:03 +0530

    updated equal split

--- splitwise/src/main/java/org/splitwise/models/split/EqualSplit.java
+++ splitwise/src/main/java/org/splitwise/models/split/EqualSplit.java
@@ -1,8 +1,45 @@
 package org.splitwise.models.split;
+import org.splitwise.models.ExpensePaidFor;
 import org.splitwise.models.Split;
 
-public class EqualSplit extends Split {
-    public EqualSplit(String userId) {
-        super(userId,0); // Amount will be set later
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EqualSplit implements ExpenseSplit {
+    List<ExpensePaidFor> expensePaidFor;
+    int totalAmount;
+    public EqualSplit(List<ExpensePaidFor> expensePaidFor, int totalAmount) {
+        this.expensePaidFor = expensePaidFor;
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public List<Split> calculateSplit(){
+
+        if(!validate()) throw new Error("Invaid share Split");
+
+        int numberOfShares = expensePaidFor.size();
+        List<Split> splits = new ArrayList<>();
+
+        for(ExpensePaidFor exp : expensePaidFor){
+            splits.add(new Split(exp.userId, totalAmount/numberOfShares));
+        }
+        return splits;
+    }
+
+    @Override
+    public boolean validate(){
+        if(expensePaidFor == null || expensePaidFor.size() == 0){
+            return false;
+        }
+        
+        int shares = 0;
+
+        for(ExpensePaidFor expense : expensePaidFor){
+            shares += expense.shares;
+        }
+
+        return  totalAmount == shares;
     }
 }
