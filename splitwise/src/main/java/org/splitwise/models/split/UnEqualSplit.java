d0c68e0b1183554c8eafdc30ea04fcbcceb16bfe
venkatarohan <venkatarohank@gmail.com>
2025-05-06 21:21:04 +0530

    updated percentage split

--- splitwise/src/main/java/org/splitwise/models/split/UnEqualSplit.java
+++ splitwise/src/main/java/org/splitwise/models/split/UnEqualSplit.java
@@ -1,8 +1,48 @@
 package org.splitwise.models.split;
+import org.splitwise.models.ExpensePaidFor;
 import org.splitwise.models.Split;
 
-public class UnEqualSplit extends Split {
-    public UnEqualSplit(String userId, int amount) {
-        super(userId, amount);
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class UnEqualSplit implements ExpenseSplit {
+    List<ExpensePaidFor> expensePaidFor;
+    int totalAmount;
+
+    public UnEqualSplit(List<ExpensePaidFor> expensePaidFor, int totalAmount) {
+        this.expensePaidFor = expensePaidFor;
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public List<Split> calculateSplit(){
+
+        if(!validate()) throw new Error("Invaid share Split");
+
+        List<Split> splits = new ArrayList<>();
+
+        for(ExpensePaidFor exp : expensePaidFor){
+            splits.add(new Split(exp.userId, exp.shares));
+        }
+        return splits;
+    }
+
+    @Override
+    public boolean validate(){
+        if(expensePaidFor == null || expensePaidFor.size() == 0){
+            return false;
+        }
+        
+        int shares = 0;
+
+        for(ExpensePaidFor expense : expensePaidFor){
+            shares += expense.shares;
+        }
+
+       
+
+        return  totalAmount == shares;
     }
 }
